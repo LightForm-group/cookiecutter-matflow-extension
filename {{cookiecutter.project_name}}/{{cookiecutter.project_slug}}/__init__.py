4fc41b39e386d95f649438161fedf50eff3aa75a
aplowman <adam.jp@live.co.uk>
2020-05-08 14:06:01 +0100

    Reorder imports in __init__

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -3,7 +3,6 @@
 from functools import partial
 
 from {{cookiecutter.project_slug}}._version import __version__
-from {{cookiecutter.project_slug}} import main
 
 from matflow import (
     input_mapper,
@@ -17,3 +16,6 @@ input_mapper = partial(input_mapper, software='{{cookiecutter.software_slug}}')
 output_mapper = partial(output_mapper, software='{{cookiecutter.software_slug}}')
 cli_format_mapper = partial(cli_format_mapper, software='{{cookiecutter.software_slug}}')
 output_files_mapper = partial(output_files_mapper, software='{{cookiecutter.software_slug}}')
+
+# This import must come after assigning the partial functions:
+from {{cookiecutter.project_slug}} import main
