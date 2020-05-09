c83a801488424026014e0213d6cf6d5523178396
aplowman <adam.jp@live.co.uk>
2020-05-09 21:07:52 +0100

    Add software key to func_mapper

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -16,6 +16,7 @@ input_mapper = partial(input_mapper, software='{{cookiecutter.software_slug}}')
 output_mapper = partial(output_mapper, software='{{cookiecutter.software_slug}}')
 cli_format_mapper = partial(cli_format_mapper, software='{{cookiecutter.software_slug}}')
 register_output_file = partial(register_output_file, software='{{cookiecutter.software_slug}}')
+func_mapper = partial(func_mapper, software='{{cookiecutter.software_slug}}')
 
 # This import must come after assigning the partial functions:
 from {{cookiecutter.project_slug}} import main
